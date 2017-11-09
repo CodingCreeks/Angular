5e512b29bf1c9a4317f9aa184b8e603bb0273b2b
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-11-08 20:46:54 -0500

    added animation

--- ng5-DesignCourse/src/app/home/home.component.ts
+++ ng5-DesignCourse/src/app/home/home.component.ts
@@ -7,10 +7,20 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
+  itemCount: Number;
+  btnText: string = 'Add an item';
+  goalText: string = 'My first life goal';
+  goals = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
